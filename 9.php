a5308a0276799f19f6fb45b0fb5c5a231e68f502
Arpit Wadhwa <120503298+Arpit-Wadhwa@users.noreply.github.com>
2025-01-30 11:50:55 +0530

    Add files via upload

---
+++ 9.php
@@ -0,0 +1,9 @@
+WAP to demonstrate resource types in php
+<?php
+
+    // Open a file for reading
+    $handle = fopen("note.txt", "r");
+    var_dump($handle);
+    echo "<br>";
+    echo "This Program is written by Arpit,2220100241"
+?>
